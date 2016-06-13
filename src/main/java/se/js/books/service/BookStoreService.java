fff3c8bf479c9963df4996c9d10914fa920a9d7d
Joran Stark <joran.stark@uadm.uu.se>
2016-06-13 15:19:30 +0200

    Implementing some functionallity for the book store service

--- src/main/java/se/js/books/service/BookStoreService.java
+++ src/main/java/se/js/books/service/BookStoreService.java
@@ -2,6 +2,7 @@ package se.js.books.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
@@ -10,5 +11,19 @@ import se.js.books.domain.Book;
 @Service
 public class BookStoreService {
 	List<Book> books = new ArrayList<>();
-
+	
+	public Book addNewBook(String author, String title, int pages) {
+		Book book = new Book(author, title, pages);
+		books.add(book);
+		return book;
+	}
+	
+	public List<Book> findAllBooks(){
+		return new ArrayList<>(books);
+	}
+	
+	public void removeBook(UUID id) {
+		books.removeIf(book -> book.getId().equals(id));
+	}
+	
 }
